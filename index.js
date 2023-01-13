72aac0c4949183f889dbb6c8166de1f9b10aac8c
andr3sc0des <andresqcodes@gmail.com>
2023-01-13 11:29:22 -0600

    changing array

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function fitsInOneBox (boxes) {
   let result = true
-  const array = boxes
-  array.sort((a, b, c) => { return (a.l * a.w * a.h) - (b.l * b.w * a.h) })
-  for (let i = 1; i < array.length; i++) {
-    if (array[i - 1].l < array[i].l && array[i - 1].w < array[i].w && array[i - 1].h < array[i].h) {
+  boxes.sort((a, b, c) => { return (a.l * a.w * a.h) - (b.l * b.w * a.h) })
+  for (let i = 1; i < boxes.length; i++) {
+    if (boxes[i - 1].l < boxes[i].l && boxes[i - 1].w < boxes[i].w && boxes[i - 1].h < boxes[i].h) {
       result = true * result
     } else {
       result = false * result
